9a02657c6c801f8a3b5108a5e05d265776e212f1
igor.chopenko <igor.chopenko@gmail.com>
2021-10-21 18:05:16 +0300

    up

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,11 +48,11 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY',
                     "django-insecure-9$6z@vjj&8k8z9fr=d0@iahfkcgcxw^9l+0eicd5y$e@1my^$p")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('DJANGO_DEBUG', True)
+DEBUG = bool(getenv('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['*']
 
-# CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 
 """
     SMTP username: Your Gmail address.
