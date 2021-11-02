0ca5876185d6ad59d8a2243742835703037815ef
Igor <igor.chopenko@gmail.com>
2021-11-03 01:35:00 +0200

    up

--- mysite/settings.py
+++ mysite/settings.py
@@ -181,8 +181,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [path.join(BASE_DIR, 'common', 'static')]
+STATIC_ROOT = path.join(BASE_DIR, 'common', 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
